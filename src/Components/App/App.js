0362ed7a5c42a3d6e6fdaf82bcc9cef5d7cc870b
Erika Miwa <erikamiwa612@gmail.com>
2019-09-26 17:30:31 -0700

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName: "",
+      playlistTracks: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+          <SearchBar/>
+          <div class="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
